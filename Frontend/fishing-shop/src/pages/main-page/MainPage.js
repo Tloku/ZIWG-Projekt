a0500aebdaca7d3f876865e9146f23dd29ce7b69
yclloud <dumitryuk.yuriy@chnu.edu.ua>
2023-04-11 19:26:21 +0200

    bestSellet and Newproducts component

--- Frontend/fishing-shop/src/pages/main-page/MainPage.js
+++ Frontend/fishing-shop/src/pages/main-page/MainPage.js
@@ -1,17 +1,26 @@
 import "../main-page/MainPage.css";
 import Product from "../../components/product/Product";
-import AdsCarousel from "../../components/adsCarousel/AdsCarousel"
+import AdsCarousel from "../../components/adsCarousel/AdsCarousel";
+import NewProducts from "../../components/newProducts/NewProducts"
+import BestSeller from "../../components/bestSellser/BestSeller";
 function MainPage() {
     let price = "2137.00zł";
   
     return (
         <div>
+            <div>
+                <BestSeller />
+            </div>
             <div className="page">
                 <Product price={price} />
             </div>
             <div className="carousel-container">
                 <AdsCarousel />
             </div>
+            <div>
+                <NewProducts />
+            </div>
+            
         </div>
         
          
